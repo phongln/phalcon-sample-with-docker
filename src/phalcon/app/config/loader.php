5d7940c74437651a87bea5cd64520da9f3dae62d
phongln <phong.le@navigosgroup.com>
2019-04-25 17:56:04 +0700

    Restructure folder src for dockerfile setup

--- src/phalcon/app/config/loader.php
+++ src/phalcon/app/config/loader.php
@@ -3,7 +3,7 @@
 $loader = new \Phalcon\Loader();
 
 // Load composer vendor stuff
-$loader->registerFiles([ BASE_PATH . "/vendor/autoload.php" ]);
+$loader->registerFiles([ BASE_PATH . "/../vendor/autoload.php" ]);
 
 /**
  * We're a registering a set of directories taken from the configuration file
